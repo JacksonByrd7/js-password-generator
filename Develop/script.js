242fe47e949c42781a4a15a4786373a97ce0e143
JacksonByrd7 <jackson.byrd7@gmail.com>
2023-11-08 14:15:37 -0500

    fillied in var arrays with special characters, capitol and lowercase letters, and numbers

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characterLength = 10
+var choiceArr = [];
+
+var specialCharArr = ["!", "@", "#", "$", "%", "*", "&"];
+var lowerCaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numberArr = ["0","1","2","3","4","5","6","7","8","9"];
+
 function generatePassword () {
-  console.log("You clicked the generate password button")
+  console.log("You clicked the generate password button");
+
+
+
+  return "Generated Password Placeholder";
 }
 // Write password to the #password input
 function writePassword() {
@@ -16,3 +28,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
